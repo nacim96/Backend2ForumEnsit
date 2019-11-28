5c7599ab2a063e15beeb2de6e618b634e17b1e71
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 19:24:01 +0100

    Update EmployeDao.java

--- src/main/java/edu/ensit/forum/Dao/EmployeDao.java
+++ src/main/java/edu/ensit/forum/Dao/EmployeDao.java
@@ -1,29 +1,20 @@
 package edu.ensit.forum.Dao;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.query.Param;
 
->>>>>>> version finale back
->>>>>>> Added my project
 import edu.ensit.forum.entity.Employe;
 
 public interface EmployeDao extends InviteRepository<Employe> {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+
 	Optional<Employe> findByUsername(String email);
     Boolean existsByEmail(String email);
 
 	 Boolean existsByIdNotAndEmail(@Param("id") long id, @Param("email") String email);
 	List<Employe> findByEmailContaining(String email);
->>>>>>> version finale back
->>>>>>> Added my project
+
 }
