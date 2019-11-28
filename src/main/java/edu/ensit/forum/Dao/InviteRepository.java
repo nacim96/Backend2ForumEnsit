f7ffbdb0a086c09f3b5e14b5d992b85f4fc6f94c
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 19:25:18 +0100

    Update InviteRepository.java

--- src/main/java/edu/ensit/forum/Dao/InviteRepository.java
+++ src/main/java/edu/ensit/forum/Dao/InviteRepository.java
@@ -7,13 +7,5 @@ import edu.ensit.forum.entity.Invite;
 public interface InviteRepository <T extends Invite> extends JpaRepository<T, Long>{
     Boolean existsByEmail(String email);
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> version finale back
->>>>>>> Added my project
 }
