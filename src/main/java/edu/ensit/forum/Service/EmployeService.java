e89602239e1c742bebef1f11e9b604c783ad3cef
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 18:58:45 +0100

    Update EmployeService.java

--- src/main/java/edu/ensit/forum/Service/EmployeService.java
+++ src/main/java/edu/ensit/forum/Service/EmployeService.java
@@ -5,13 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 import edu.ensit.forum.entity.Employe;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 import edu.ensit.forum.entity.Etudiant;
->>>>>>> version finale back
->>>>>>> Added my project
+
 
 public interface EmployeService {
 	
@@ -19,17 +14,10 @@ public interface EmployeService {
 	ResponseEntity<?> save(Employe employe);
 	
     List<Employe> findAll();
-<<<<<<< HEAD
-    
-=======
-<<<<<<< HEAD
-    
-=======
+
     Employe findById(Long id);
 	void delete(Long id);
 	 ResponseEntity<?> updateInvite(Employe employe,long id);
->>>>>>> version finale back
->>>>>>> Added my project
-   
+
 
 }
