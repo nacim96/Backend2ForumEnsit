348f86cddee99887e2b8b133340cdcb56438228f
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 19:29:57 +0100

    Update EtudiantDao.java

--- src/main/java/edu/ensit/forum/Dao/EtudiantDao.java
+++ src/main/java/edu/ensit/forum/Dao/EtudiantDao.java
@@ -3,7 +3,6 @@ package edu.ensit.forum.Dao;
 
 import edu.ensit.forum.entity.Etudiant;
 
-public interface EtudiantDao  extends InviteRepository<Etudiant>{
 
 import java.util.List;
 import java.util.Optional;
