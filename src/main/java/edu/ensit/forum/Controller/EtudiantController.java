af9a8e8f40f9fed9b954d2db1fc70dfb6589fd4d
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 19:19:36 +0100

    Update EtudiantController.java

--- src/main/java/edu/ensit/forum/Controller/EtudiantController.java
+++ src/main/java/edu/ensit/forum/Controller/EtudiantController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
