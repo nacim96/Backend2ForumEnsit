ade7871681ec2faa96f45fd5e8e00550c1b172de
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-28 18:58:06 +0100

    Update EntrepriseService.java

--- src/main/java/edu/ensit/forum/Service/EntrepriseService.java
+++ src/main/java/edu/ensit/forum/Service/EntrepriseService.java
@@ -5,26 +5,18 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 import edu.ensit.forum.entity.Entreprise;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+
 import edu.ensit.forum.entity.Etudiant;
->>>>>>> version finale back
->>>>>>> Added my project
+
 
 public interface EntrepriseService {
 	ResponseEntity<?> save(Entreprise entreprise);
 
 	List<Entreprise> findAll();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+
 	Entreprise findById(Long id);
 	void delete(Long id);
 	 ResponseEntity<?> updateInvite(Entreprise entreprise,long id);
->>>>>>> version finale back
->>>>>>> Added my project
+
 
 }
