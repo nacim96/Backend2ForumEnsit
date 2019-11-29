f094cd8d3662016c4461f8578aca8dcc3cf912ef
nacim96 <55284419+nacim96@users.noreply.github.com>
2019-11-29 16:27:20 +0100

    Update InsigthOfEnsitApplicationTests.java

--- src/test/java/edu/ensit/miniprojet/InsigthOfEnsitApplicationTests.java
+++ src/test/java/edu/ensit/miniprojet/InsigthOfEnsitApplicationTests.java
@@ -1,9 +1,9 @@
 package edu.ensit.miniprojet;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class InsigthOfEnsitApplicationTests {
 
 	@Test
